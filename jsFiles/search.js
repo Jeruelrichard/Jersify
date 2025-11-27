37d7057df4000f4b3406163e9c5744a56ace9109
Jeruelrichard <okemdinach383@gmail.com>
2025-11-28 00:00:34 +0100

    search btns functioning [me coded :-)]

--- jsFiles/search.js
+++ jsFiles/search.js
@@ -1,4 +1,32 @@
-//I need to work on all these
+//Opens Search form
+function openSearch() {
+  document.querySelector(".searchArea").style.display = "block";
+  document.getElementById("overlay").style.display = "block";
+document.body.classList.add("no-scroll")}
+
+//Closes Search form
+function closeSearch() {
+    document.querySelector(".searchArea").style.display="none";
+    document.getElementById("overlay").style.display= "none";
+document.body.classList.remove("no-scroll")
+}
+
+//Closes Search when overlay is clicked
+function closeSearchOnBackdrop(event) {
+    if (event.target.id === 'overlay') {
+        closeSearch();
+    }
+}
+
+//Closes Search when you press esc on keyboard
+document.addEventListener('keydown', function (event) {
+    if (event.key === 'Escape') {
+        closeSearch();
+    }
+});
+
+
+//I need to work on all these below
 
 const searchForm = document.getElementById('searchForm');
 const searchInput = document.getElementById('searchInput');
@@ -24,10 +52,3 @@ searchForm.addEventListener('submit', function(e) {
       e.preventDefault();
       filterCards(searchInput.value);
     });
-
-function closeBtn() {
-    const closeBtn = document.getElementById("searchBar");
-    closeBtn.addEventListener("click", () => {
-        document.body.searchBar.style.display.none
-    })
-}
